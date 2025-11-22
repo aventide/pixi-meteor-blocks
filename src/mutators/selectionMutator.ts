c8c0da6f03c7a919031af7ca2415677aa9b457d6
Alex Staples <ajstaples@gmail.com>
2025-11-22 00:40:14 -0500

    refactor: move block graphic change inside of conditional

--- src/mutators/selectionMutator.ts
+++ src/mutators/selectionMutator.ts
@@ -20,27 +20,6 @@ export const selectionMutator = (blockGroup: BlockGroup) => {
     setSelectedBlockGroup(null);
   }
 
-  if (selectedBlock) {
-    if (globalPointer.y < selectedBlock.sprite.y) {
-      const fileBlockDirectlyAbove = getFileBlockDirectlyAbove({
-        block: selectedBlock,
-        blockGroupId: blockGroup.id,
-      });
-      if (fileBlockDirectlyAbove) {
-        swapFileBlockPositions(selectedBlock, fileBlockDirectlyAbove);
-      }
-    }
-    if (globalPointer.y > selectedBlock.sprite.y + blockSize) {
-      const fileBlockDirectlyBelow = getFileBlockDirectlyBelow({
-        block: selectedBlock,
-        blockGroupId: blockGroup.id,
-      });
-      if (fileBlockDirectlyBelow) {
-        swapFileBlockPositions(selectedBlock, fileBlockDirectlyBelow);
-      }
-    }
-  }
-
   blockGroup.files.forEach((file) => {
     // check for danger condition on group file
     if (file.blocks.length > DEFAULT_FILE_LIMIT) {
@@ -55,8 +34,36 @@ export const selectionMutator = (blockGroup: BlockGroup) => {
       blockGroup.id === selectedBlockGroup?.id;
 
     file.blocks.forEach((block) => {
-      block.sprite.alpha =
-        selectedBlock?.sprite.uid === block.sprite.uid ? 0.5 : 1;
+      // if current block being iterated is the selected block
+      if (selectedBlock && selectedBlock.sprite.uid === block.sprite.uid) {
+        block.sprite.alpha = 0.5;
+
+        // if pointer moves relative to selected block, handle block swap
+        if (globalPointer.y < selectedBlock.sprite.y) {
+          const fileBlockDirectlyAbove = getFileBlockDirectlyAbove({
+            block: selectedBlock,
+            blockGroupId: blockGroup.id,
+          });
+          if (fileBlockDirectlyAbove) {
+            swapFileBlockPositions(selectedBlock, fileBlockDirectlyAbove);
+          } else {
+            // blockGroup.velocity = DEFAULT_POP_VELOCITY;
+          }
+        }
+        if (globalPointer.y > selectedBlock.sprite.y + blockSize) {
+          const fileBlockDirectlyBelow = getFileBlockDirectlyBelow({
+            block: selectedBlock,
+            blockGroupId: blockGroup.id,
+          });
+          if (fileBlockDirectlyBelow) {
+            swapFileBlockPositions(selectedBlock, fileBlockDirectlyBelow);
+          }
+        }
+      } else {
+        block.sprite.alpha = 1;
+      }
+
+      // set selection if a file block is being hovered over
       if (
         globalPointer.x >= block.sprite.x &&
         globalPointer.x <= block.sprite.x + blockSize &&
