27d74acfe9293eff85e71d34f34b3515165f7ef5
Alex Staples <ajstaples@gmail.com>
2025-12-27 21:28:27 -0500

    feat: concept for rate-limiting descent of launched groups

--- src/mutators/descentMutator.ts
+++ src/mutators/descentMutator.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_LAUNCH_DESCENT_RATE } from "../constants";
 import type { BlockGroup } from "../entities/types";
 
 import { getWorld } from "../world";
@@ -5,7 +6,13 @@ import { getWorld } from "../world";
 export const descentMutator = (blockGroup: BlockGroup, dt: number) => {
   const { gravity } = getWorld();
 
-  // if blockGroup is a launch group (or within one), then set velocity to a constant descent rate
-  // otherwise, apply acceleration due to gravity
   blockGroup.velocity += gravity * dt;
+
+  // if blockGroup is a launch group (or within one), then cap downward velocity to a max speed
+  // if (blockGroup.type === "launch" && blockGroup.velocity > 0) {
+  //   blockGroup.velocity = Math.min(
+  //     blockGroup.velocity,
+  //     DEFAULT_LAUNCH_DESCENT_RATE,
+  //   );
+  // }
 };
