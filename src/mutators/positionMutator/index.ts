325b160a5ca6d5c9dc528676b2af2432159b0c8c
Alex Staples <ajstaples@gmail.com>
2026-02-21 17:04:36 -0500

    feat: added rudimentary contact handling

--- src/mutators/positionMutator/index.ts
+++ src/mutators/positionMutator/index.ts
@@ -5,6 +5,12 @@ import { BlockGroup } from "../../entities/types";
 import { getFloor, getWorld } from "../../world";
 import { getClosestFragmentBelow, translatePosition } from "./util";
 
+type ContactType = "contacted-floor" | "contacted-group";
+type Contact = {
+  type: ContactType;
+  contactedGroups?: BlockGroup[];
+};
+
 const positionMutator = (group: BlockGroup, dt: number) => {
   const { height: worldHeight } = getWorld();
   const floor = getFloor();
@@ -20,13 +26,15 @@ const positionMutator = (group: BlockGroup, dt: number) => {
   const movementDiff = velocity * dt * (worldHeight / DEFAULT_REFERENCE_HEIGHT);
 
   let minDiff = movementDiff;
+  let contact: Contact | null = null;
 
   // diffs to obstacles below - check each fragment
-  group.fileFragments.forEach((frag) => {
+  for (const frag of group.fileFragments) {
     // calculate and store diff to floor for this frag
     const diffToFloor = floor - frag.boundary.bottom;
     if (diffToFloor < minDiff) {
       minDiff = diffToFloor;
+      contact = { type: "contacted-floor" };
     }
 
     // then calculate and track diffs to any fragments below in this file
@@ -35,11 +43,22 @@ const positionMutator = (group: BlockGroup, dt: number) => {
       const diffToClosestFragment =
         closestFragmentBelow.boundary.top - frag.boundary.bottom;
 
-      minDiff = Math.min(diffToClosestFragment, minDiff);
+      if (diffToClosestFragment < minDiff) {
+        minDiff = diffToClosestFragment;
+        contact = { type: "contacted-group" };
+      }
     }
-  });
+  }
 
   translatePosition(group, minDiff);
+
+  if (contact) {
+    if (contact.type === "contacted-floor") {
+      group.velocity = 0;
+    } else if (contact.type === "contacted-group" && group.type !== "launch") {
+      group.velocity = 0;
+    }
+  }
 };
 
 export default positionMutator;
